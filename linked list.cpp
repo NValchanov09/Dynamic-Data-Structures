6f8396b2abc2f07f7676bb8e308f395736745e1b
Nikolay Valchanov <158312030+NValchanov09@users.noreply.github.com>
2024-02-15 16:48:24 +0200

    Added Linked List C++
    Added Linked List Pure Source Code in C++. (No test)

---
+++ linked list.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename Type>
+class Node
+{
+public:
+
+    Type data;
+    Node *next;
+
+    Node();
+};
+
+/// Constructor for Node - Sets initial values / Creates the data structure
+
+template <typename Type>
+Node<Type> :: Node()
+{
+    data = 0;
+    next = nullptr;
+}
+
+template <typename Type>
+class LinkedList
+{
+private:
+
+    Node<Type>* head;
+    int list_size;
+
+public:
+
+    LinkedList();
+    ~LinkedList();
+    bool empty();
+    int size();
+    Type front();
+    Type* begin();
+    void insert(Type);
+    void remove();
+    void clear();
+};
+
+/// Constructor for LinkedList - Sets initial values / Creates the data structure
+
+template <typename Type>
+LinkedList<Type> :: LinkedList()
+{
+    head = nullptr;
+    list_size = 0;
+}
+
+/// Destructor for LinkedList - Deletes the whole data structure after the end of the execution
+
+template <typename Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    clear();
+}
+
+/// Returns true or false if the LinkedList is empty or not
+
+template <typename Type>
+bool LinkedList<Type> :: empty()
+{
+    return list_size == 0;
+}
+
+
+/// Returns the size of the LinkedList
+
+template <typename Type>
+int LinkedList<Type> :: size()
+{
+    return list_size;
+}
+
+/// Returns the value of the first element / head of the LinkedList
+
+template <typename Type>
+Type LinkedList<Type> :: front()
+{
+    return head -> data;
+}
+
+/// Return a pointer / iterator to the first element / head of the LinkedList
+
+template <typename Type>
+Type* LinkedList<Type> :: begin()
+{
+    return head;
+}
+
+/// Inserts the given value to the beginning of the LinkedList
+
+template <typename Type>
+void LinkedList<Type> :: insert(Type value)
+{
+    Node<Type>* ptr = new Node<Type>();
+    ptr -> data = value;
+    ptr -> next = head;
+    head = ptr;
+    list_size++;
+}
+
+/// Removes the first element of the LinkedList
+
+template <typename Type>
+void LinkedList<Type> :: remove()
+{
+    Node<Type>* ptr = head;
+    head = head -> next;
+    delete ptr;
+    list_size--;
+}
+
+/// Clears the LinkedList / Removes all elements
+
+template <typename Type>
+void LinkedList<Type> :: clear()
+{
+    while(!empty())
+        remove();
+}
+
+int main()
+{
+
+	return 0;
+}
